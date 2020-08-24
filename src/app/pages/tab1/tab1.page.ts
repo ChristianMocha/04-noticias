01f52988cca38cf479ef57d84300b9a4c5691f90
ChristianMocha <letmy.4@hotmail.com>
2020-08-24 14:53:30 -0500

    mostrar los articulos de noticias

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NoticiasService } from '../../services/noticias.service';
+import { Article } from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +9,8 @@ import { NoticiasService } from '../../services/noticias.service';
 })
 export class Tab1Page implements OnInit{
 
+  noticias: Article[]=[];
+
   constructor(private noticiasService: NoticiasService) {
 
   }
@@ -15,6 +18,8 @@ export class Tab1Page implements OnInit{
     this.noticiasService.getTopHeadlines().subscribe( 
       resp => {     
         console.log('noticias ', resp);
+        //this.noticias = resp.articles;
+        this.noticias.push( ...resp.articles );
     })
   }
 
--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaTopHeadlines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadlines() {
-    return this.http.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=29be717171a24de3904107dcd0427982`);
+    return this.http.get<RespuestaTopHeadlines>(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=29be717171a24de3904107dcd0427982`);
   }
 }
