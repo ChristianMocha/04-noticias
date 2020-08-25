3b3e706dbb8f4c1f2ddfd82c81566b3add429890
ChristianMocha <letmy.4@hotmail.com>
2020-08-25 14:30:23 -0500

    mostrar action sheet en noticias

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
+
 @Component({
   selector: 'app-noticia',
   templateUrl: './noticia.component.html',
@@ -11,8 +14,44 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor() { }
+  constructor(private iab: InAppBrowser, private actionSheetCrtl: ActionSheetController
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  abrirNoticia() {
+    //console.log("noticia", this.noticia.url);
+    const browser = this.iab.create(this.noticia.url, '_system');
+
+  }
+  async lanzarMenu() {
+
+    const actionSheet = await this.actionSheetCrtl.create({
+
+
+      buttons: [{
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('favorite');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+  }
 
 }
